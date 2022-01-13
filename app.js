dd5ce1754e5eaa7480df81c4f3bd88fe4a9956ce
Carlos Adrian Catindig <adriancatindig1216@gmail.com>
2022-01-13 17:14:24 +0800

    Integrated User Model, Views, and Passport

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const app = express();
 const ejsMate = require('ejs-mate');
 const path = require('path');
 const methodOverride = require('method-override');
+const userRoutes = require('./routes/users');
 const restaurantRoutes = require('./routes/restaurants');
 const reviewRoutes = require('./routes/reviews');
 const AppError = require('./utils/AppError');
 const session = require('express-session');
 const flash = require('connect-flash');
+const User = require('./models/user');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 
 const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
@@ -45,12 +49,21 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
+    res.locals.path = req.path;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 });
 
+app.use('/', userRoutes);
 app.use('/restaurants', restaurantRoutes);
 app.use('/restaurants/:id/reviews', reviewRoutes);
 
